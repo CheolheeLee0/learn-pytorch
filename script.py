b441c4214bc6174c65286e66df2759514332f5f0
CheolheeLee0 <wks0968@gmail.com>
2024-08-09 01:03:25 +0900

    feat: script

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 from huggingface_hub import hf_hub_download
 import joblib
 
-REPO_ID = "CheolheeLee/new-project"
-FILENAME = "sklearn_model.joblib"
+REPO_ID = "meta-llama"
+FILENAME = "Meta-Llama-3.1-8B-Instruct"
 
 model = joblib.load(
     hf_hub_download(repo_id=REPO_ID, filename=FILENAME)
