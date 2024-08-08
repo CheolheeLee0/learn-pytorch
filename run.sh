c11bd673b8938f8c333b18aa1a4e2e09affbbc41
CheolheeLee0 <wks0968@gmail.com>
2024-08-09 00:27:26 +0900

    feat: toml

--- run.sh
+++ run.sh
@@ -1,14 +1,2 @@
-# 3.9.18
-python3.9 -m venv venv
-source venv/bin/activate
-pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
-pip install --no-deps "xformers<0.0.27" "trl<0.9.0" peft accelerate bitsandbytes
-
-pip install -r requirements.txt
-
-
-pip list --outdated
-pip freeze > requirements.txt
-pip install -r requirements.txt --upgrade
 
 python3 llama_fine_tuning.py
