ecfaf95a9f8d39da78684f11aec4e8b5bb391bff
CheolheeLee0 <wks0968@gmail.com>
2024-08-09 01:03:49 +0900

    feat: script

--- script.py
+++ script.py
@@ -1,11 +1,22 @@
-from huggingface_hub import hf_hub_download
-import joblib
+import transformers
+import torch
 
-REPO_ID = "meta-llama"
-FILENAME = "Meta-Llama-3.1-8B-Instruct"
+model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 
-model = joblib.load(
-    hf_hub_download(repo_id=REPO_ID, filename=FILENAME)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    device_map="auto",
 )
 
-print(model.predict([[1, 2, 3]]))
+messages = [
+    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {"role": "user", "content": "Who are you?"},
+]
+
+outputs = pipeline(
+    messages,
+    max_new_tokens=256,
+)
+print(outputs[0]["generated_text"][-1])
