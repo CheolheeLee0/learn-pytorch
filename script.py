03dddd9cd0255d89ae21e84ce986294734479d13
CheolheeLee0 <wks0968@gmail.com>
2024-08-09 01:08:18 +0900

    feat: h100

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 pipeline = transformers.pipeline(
     "text-generation",
     model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16},
+    model_kwargs={"torch_dtype": torch.bfloat16, "rope_scaling": {"type": "dynamic", "factor": 8.0}},
     device_map="auto",
 )
 
@@ -20,3 +20,4 @@ outputs = pipeline(
     max_new_tokens=256,
 )
 print(outputs[0]["generated_text"][-1])
+# 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
