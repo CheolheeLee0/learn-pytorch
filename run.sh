9a4b58d6fd2d571404ec9786e2676be0aee70513
CheolheeLee0 <wks0968@gmail.com>
2024-08-09 00:28:35 +0900

    feat: force reinstall

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
+pip install --force-reinstall python==3.9.18
+pip install --force-reinstall unsloth==0.4.1
+pip install --force-reinstall torch==2.0.0
+pip install --force-reinstall trl==0.4.1
+pip install --force-reinstall transformers==4.27.4
+pip install --force-reinstall datasets==2.11.0
 
 python3 llama_fine_tuning.py
