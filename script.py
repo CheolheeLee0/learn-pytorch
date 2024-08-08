d26a4b199e0c1c262d0780f10644052e35d2d2fd
CheolheeLee0 <wks0968@gmail.com>
2024-08-09 01:11:32 +0900

    feat: h100

--- script.py
+++ script.py
@@ -6,9 +6,8 @@ model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
 pipeline = transformers.pipeline(
     "text-generation",
     model=model_id,
-    # model_kwargs={"torch_dtype": torch.bfloat16, "rope_scaling": {"type": "dynamic", "factor": 8.0}},
+    model_kwargs={"torch_dtype": torch.bfloat16, "rope_scaling": {"type": "dynamic", "factor": 8.0}},
     device_map="auto",
-    trust_remote_code=True  # Add this line to trust remote code
 )
 
 messages = [
