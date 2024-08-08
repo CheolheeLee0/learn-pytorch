b8d83eff36c68f95ffcda589e75d813c7e4a519b
CheolheeLee0 <wks0968@gmail.com>
2024-08-09 05:39:44 +0900

    fix: file name

--- train_japanese.py
+++ train_japanese.py
@@ -1,6 +1,6 @@
+from datasets import load_dataset 
 import time
 import pandas as pd
-from train_japanese import load_dataset 
 import transformers
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer
