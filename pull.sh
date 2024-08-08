cf3cca901375c4c6573199c19e2c4a5c03dbaeb5
CheolheeLee0 <wks0968@gmail.com>
2024-08-09 02:47:13 +0900

    feat: s3_downlaod

--- pull.sh
+++ pull.sh
@@ -1,5 +1,4 @@
 git pull
-pip install boto3 botocore os
 
 
 python3 s3.py
--- s3_download.py
+++ s3_download.py
@@ -4,16 +4,9 @@ import os
 import time
 import transformers
 import torch
-import zipfile
+import tarfile
 
 model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
-
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model_id,
-    model_kwargs={"torch_dtype": torch.bfloat16, "rope_scaling": {"type": "dynamic", "factor": 8.0}},
-    device_map="auto",
-)
 
 
 # S3 버킷과 객체 정보
@@ -25,33 +18,6 @@ folder_name = 'models--meta-llama--Meta-Llama-3.1-8B-Instruct'
 
 # AWS S3 클라이언트 생성
 s3 = boto3.client('s3')
-
-def zip_directory(directory_path, zip_path):
-    with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for root, dirs, files in os.walk(directory_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                zipf.write(file_path, os.path.relpath(file_path, directory_path))
-
-def upload_with_progress(bucket_name, object_key, source_file):
-    try:
-        with open(source_file, 'rb') as f:
-            start_time = time.time()
-            total_bytes = os.path.getsize(source_file)
-
-            def progress_callback(bytes_amount):
-                elapsed_time = time.time() - start_time
-                speed = bytes_amount / elapsed_time if elapsed_time > 0 else 0
-                print(f"\rUploaded {bytes_amount} bytes at {speed:.2f} bytes/sec", end='')
-
-            s3.upload_fileobj(f, bucket_name, object_key, Callback=progress_callback)
-            print(f"\n파일이 성공적으로 업로드되었습니다: {source_file}")
-    except botocore.exceptions.NoCredentialsError:
-        print("AWS 자격 증명이 설정되지 않았습니다.")
-    except botocore.exceptions.PartialCredentialsError:
-        print("AWS 자격 증명이 불완전합니다.")
-    except Exception as e:
-        print(f"파일 업로드 중 오류가 발생했습니다: {e}")
 
 def download_with_progress(bucket_name, object_key, destination_file):
     try:
@@ -76,20 +42,11 @@ def download_with_progress(bucket_name, object_key, destination_file):
         print(f"파일 다운로드 중 오류가 발생했습니다: {e}")
 
 def main():
-    zip_path = os.path.join(os.getcwd(), 'hpc_hub.zip')
-    download_with_progress(bucket_name, 'home_hub.zip', zip_path)
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-        zip_ref.extractall(hpc_hub_path)
-
     # 모델 파일 다운로드 및 압축 풀기
-    model_zip_path = os.path.join(os.getcwd(), 'model.zip')
-    download_with_progress(bucket_name, 'model/model.bin', model_zip_path)
-    with zipfile.ZipFile(model_zip_path, 'r') as zip_ref:
-        zip_ref.extractall(os.path.join(hpc_hub_path, folder_name))
-
-model_file = os.path.join(os.getcwd(), hpc_hub_path + 'model.bin')
-pipeline.save_pretrained(model_file)
-download_with_progress(bucket_name, 'model/model.bin', model_file)
-
+    model_zip_path = os.path.join(hpc_hub_path, 'hub.tar')
+    download_with_progress(bucket_name, 'home_hub.tar', model_zip_path)
+    with tarfile.open(model_zip_path, 'r') as tar:
+        tar.extractall(hpc_hub_path)
+        
 if __name__ == "__main__":
     main()
