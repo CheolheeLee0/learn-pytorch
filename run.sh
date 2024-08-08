9421ca1a5295cca8c5be80bbbe243a2287c6e8fd
CheolheeLee0 <wks0968@gmail.com>
2024-08-08 23:51:06 +0900

    feat: run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
+# 3.9.18
 python3.9 -m venv venv
 source venv/bin/activate
 pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
 pip install --no-deps "xformers<0.0.27" "trl<0.9.0" peft accelerate bitsandbytes
 
-pip install -r requirements.txt && python3 run.py
+pip install -r requirements.txt
+
+python3 run.py
